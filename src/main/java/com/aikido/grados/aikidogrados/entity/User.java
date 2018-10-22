47cdd092a4ef5f2519fd04f31a71743434f026e1
Beatriz Lopez <beatlm@gmail.com>
2018-10-22 23:34:24 +0200

    status and dates

--- src/main/java/com/aikido/grados/aikidogrados/entity/User.java
+++ src/main/java/com/aikido/grados/aikidogrados/entity/User.java
@@ -10,8 +10,15 @@ public class User {
 
 	private String name;
 	private String email;
-	private List<UserStatus> status;
+	private String status;
 	private String grado;
+	private String createDate;
+	private String emailDate;
+	private String paymentDate;
+	private String sentDate;
+	private String receivedDate;
+	private String file;
+	private String paymentFile;
 
 	public String getId() {
 		return id;
@@ -38,13 +45,53 @@ public class User {
 	public void setGrado(String grado) {
 		this.grado = grado;
 	}
-	public List<UserStatus> getStatus() {
+	public String getStatus() {
 		return status;
 	}
-	public void setStatus(List<UserStatus> status) {
+	public void setStatus(String status) {
 		this.status = status;
 	}
-
-	
+	public String getCreateDate() {
+		return createDate;
+	}
+	public void setCreateDate(String createDate) {
+		this.createDate = createDate;
+	}
+	public String getEmailDate() {
+		return emailDate;
+	}
+	public void setEmailDate(String emailDate) {
+		this.emailDate = emailDate;
+	}
+	public String getPaymentDate() {
+		return paymentDate;
+	}
+	public void setPaymentDate(String paymentDate) {
+		this.paymentDate = paymentDate;
+	}
+	public String getSentDate() {
+		return sentDate;
+	}
+	public void setSentDate(String sentDate) {
+		this.sentDate = sentDate;
+	}
+	public String getReceivedDate() {
+		return receivedDate;
+	}
+	public void setReceivedDate(String receivedDate) {
+		this.receivedDate = receivedDate;
+	}
+	public String getFile() {
+		return file;
+	}
+	public void setFile(String file) {
+		this.file = file;
+	}
+	public String getPaymentFile() {
+		return paymentFile;
+	}
+	public void setPaymentFile(String paymentFile) {
+		this.paymentFile = paymentFile;
+	}
 
 }
