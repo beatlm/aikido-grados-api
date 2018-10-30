b231d988ebf30d5bd1c9f8c71e26e09570ab0e55
Beatriz Lopez <beatlm@gmail.com>
2018-10-30 22:50:06 +0100

    licence search pageable

--- src/main/java/com/aikido/grados/aikidogrados/repository/UserRepository.java
+++ src/main/java/com/aikido/grados/aikidogrados/repository/UserRepository.java
@@ -3,6 +3,8 @@ package com.aikido.grados.aikidogrados.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
@@ -24,6 +26,6 @@ public interface UserRepository extends PagingAndSortingRepository<User,String> 
 	List<User> findByName(@Param("name") String name); 
 
 	@RestResource( rel="buscarPorLicencia", path="findByLicenceNumber" )
-	List<User> findByLicence(@Param("licence") String licence); 
+	Page<User> findByLicence(@Param("licence") String licence, Pageable pageable); 
 	
 }
