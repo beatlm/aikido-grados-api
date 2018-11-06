9b59ddd3e9802ef53965e4438a4c1a8e7938c90e
blopezmo <blopezmo@vectoritcgroup.com>
2018-11-06 13:26:26 +0100

    @RequestMapping("/api")

--- src/main/java/com/aikido/grados/aikidogrados/repository/UserRepository.java
+++ src/main/java/com/aikido/grados/aikidogrados/repository/UserRepository.java
@@ -8,6 +8,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.aikido.grados.aikidogrados.entity.User;
@@ -15,7 +16,7 @@ import com.aikido.grados.aikidogrados.entity.User;
 
 
 @RepositoryRestResource(collectionResourceRel = "result", path = "user") 
-
+@RequestMapping("/api")
 @CrossOrigin(origins = {"http://localhost:4200", "https://aikido-grados.herokuapp.com"}, 
 methods={RequestMethod.GET, RequestMethod.DELETE, RequestMethod.POST, RequestMethod.PATCH})
 public interface UserRepository extends PagingAndSortingRepository<User,String> {
