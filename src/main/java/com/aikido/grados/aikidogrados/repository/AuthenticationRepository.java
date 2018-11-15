861b834cb013bdae7289267666daf68a98106cab
blopezmo <blopezmo@vectoritcgroup.com>
2018-11-15 11:58:38 +0100

    removed find by user and password

--- src/main/java/com/aikido/grados/aikidogrados/repository/AuthenticationRepository.java
+++ src/main/java/com/aikido/grados/aikidogrados/repository/AuthenticationRepository.java
@@ -7,5 +7,5 @@ import com.aikido.grados.aikidogrados.model.AuthenticateUser;
 public interface AuthenticationRepository extends CrudRepository<AuthenticateUser, String> {
 
 	public AuthenticateUser findByUsername(String username);
-	public AuthenticateUser findByUsernameAndPassword(String username, String password);
+
 }
