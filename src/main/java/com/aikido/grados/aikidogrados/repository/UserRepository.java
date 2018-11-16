5e70f07565b5bd2e3d469bc2395955ca523b82bd
blopezmo <blopezmo@vectoritcgroup.com>
2018-11-16 11:32:36 +0100

    request mapping

--- src/main/java/com/aikido/grados/aikidogrados/repository/UserRepository.java
+++ src/main/java/com/aikido/grados/aikidogrados/repository/UserRepository.java
@@ -13,22 +13,16 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import com.aikido.grados.aikidogrados.entity.User;
 
 
-@RequestMapping("/api/user")
-@RepositoryRestResource(collectionResourceRel = "result", path = "user") 
+@RequestMapping("/api")
+@RepositoryRestResource(collectionResourceRel = "result", path = "/api/user") 
 @CrossOrigin(origins = {"http://localhost:4200", "http://localhost:8080","https://aikido-grados.herokuapp.com"}, 
 methods={RequestMethod.GET, RequestMethod.DELETE, RequestMethod.POST, RequestMethod.PATCH})
 public interface UserRepository extends CrudRepository<User,String> {
-
-	/*@RestResource( rel="buscarPorLicencia", path="findByLicenceNumber" )
-	public List<User> findByLicence(@Param("licence") String licence);*/ 
 	
 	@Query(value="{'licence': {$regex: ?0 , $options: 'i' }})")
 	public List<User> findByLicenceQuery(String name);
 
 	@Query(value="{'name': {$regex: ?0 , $options: 'i' }})")
 	public List<User> findByNameQuery(String name);
-
-	//@RestResource(  path="/api/user/findAll" )
-	//public List<User> findAll();
 
 }
